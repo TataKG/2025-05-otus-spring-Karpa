21667acc940efbb7ccb850ed8735f9ce20f0a37d
TataKG <tania_1788@list.ru>
2025-06-10 09:52:43 +0300

    Добавление основного фунукционала и настроек jar

--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,11 +1,18 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
+// Настроить связи между объектами внутри модуля
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+// Добавить библиотеки для работы с потоками
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
@@ -18,6 +25,29 @@ public class CsvQuestionDao implements QuestionDao {
         // Использовать QuestionReadException
         // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
 
-        return new ArrayList<>();
+        ClassLoader loader = getClass().getClassLoader();
+        InputStream resource = loader.getResourceAsStream(fileNameProvider.getTestFileName());
+
+        // Проверить, что ресурс был сформирован
+        if (resource ==  null){
+            throw new QuestionReadException(
+                    String.format("File access error - \"%s\"",fileNameProvider.getTestFileName()));
+        }
+
+        try {
+            List<QuestionDto> questionsDto = new CsvToBeanBuilder<QuestionDto>(
+                    new InputStreamReader(resource))
+                    .withSkipLines(1)
+                    .withSeparator(';')
+                    .withType(QuestionDto.class)
+                    .build().parse();
+
+            return questionsDto.stream()
+                    .map(QuestionDto::toDomainObject)
+                    .collect(Collectors.toList());
+
+        } catch (RuntimeException e) {
+            throw new QuestionReadException("File reading error", e);
+        }
     }
 }
--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,42 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
+    private final QuestionDao questionDao;
 
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
-        // Получить вопросы из дао и вывести их с вариантами ответов
+        // Получить вопросы из дао и вывести их с вариантами ответ
+        List<Question> questionList = questionDao.findAll();
+        // Провести тестирование
+        startTest(questionList);
+    }
+
+    private void startTest(List<Question> questionList){
+        int questionIndex = 0;
+        int answerIndex;
+
+        for (Question question : questionList){
+            questionIndex++;
+            // Текст вопроса
+            ioService.printLine(questionIndex + ". " + question.text());
+            // Варианты ответов
+            answerIndex = 0;
+            for (Answer answer: question.answers()){
+                answerIndex++;
+                ioService.printLine("  " + questionIndex + "." + answerIndex + ". " + answer.text());
+            }
+        }
     }
 }
