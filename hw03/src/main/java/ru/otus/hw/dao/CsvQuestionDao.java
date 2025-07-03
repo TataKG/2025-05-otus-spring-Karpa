4d50440892657e27f8f6fec40f229fd25c474289
TataKG <tania_1788@list.ru>
2025-07-03 17:10:48 +0300

    Начало адаптации проекта

--- hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -11,15 +11,42 @@ import java.util.List;
 @RequiredArgsConstructor
 @Component
 public class CsvQuestionDao implements QuestionDao {
+
+    private static final int COUNT_LINES_SKIP = 1;
+
+    private static final char SEPARATOR = ';';
+
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
-
-        return new ArrayList<>();
+        try (InputStream resource = getClass().getClassLoader().getResourceAsStream(
+                fileNameProvider.getTestFileName())) {
+            return readStreamToList(resource);
+        } catch (IOException | RuntimeException e) {
+            throw new QuestionReadException("Error Reading File", e);
+        }
     }
+
+    private List<Question> readStreamToList(InputStream resource) {
+        try (var reader = new BufferedReader(new InputStreamReader(resource))) {
+
+            CsvToBean<QuestionDto> csvToBean = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withSeparator(SEPARATOR)
+                    .withSkipLines(COUNT_LINES_SKIP)
+                    .withType(QuestionDto.class)
+                    .withOrderedResults(true)
+                    .build();
+
+            List<QuestionDto> questionsDto = csvToBean.stream().toList();
+            if (questionsDto.isEmpty()) {
+                throw new QuestionReadException("Question List is empty");
+            }
+            return questionsDto.stream().map(QuestionDto::toDomainObject).toList();
+
+        } catch (RuntimeException | IOException e) {
+            throw new QuestionReadException("Parsing error", e);
+        }
+    }
+
 }
--- hw03/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw03/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,6 +3,7 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
