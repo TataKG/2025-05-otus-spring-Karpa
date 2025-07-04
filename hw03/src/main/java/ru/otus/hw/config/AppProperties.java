ba6639fda6c68f3cd12e811fefbe93508bfdbca8
TataKG <tania_1788@list.ru>
2025-07-04 12:22:41 +0300

    Корректировки AppProperties

--- hw03/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw03/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -1,27 +1,30 @@
 package ru.otus.hw.config;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.Locale;
 import java.util.Map;
 
-@Setter
+@Data
 @ConfigurationProperties(prefix = "test")
 public class AppProperties implements TestConfig, TestFileNameProvider, LocaleConfig {
 
-    @Getter
     private int rightAnswersCountToPass;
 
-    @Getter
     private Locale locale;
 
     private Map<String, String> fileNameByLocaleTag;
 
-    public void setLocale(String locale) {
+    public AppProperties(
+            @Value("${test.rightAnswersCountToPass}") int rightAnswersCountToPass,
+            @Value("${test.locale}") String locale,
+            @Value("${test.fileNameByLocaleTag}") Map<String, String> fileNameByLocaleTag) {
+        this.rightAnswersCountToPass = rightAnswersCountToPass;
         this.locale = Locale.forLanguageTag(locale);
+        this.fileNameByLocaleTag = fileNameByLocaleTag;
     }
 
     @Override
