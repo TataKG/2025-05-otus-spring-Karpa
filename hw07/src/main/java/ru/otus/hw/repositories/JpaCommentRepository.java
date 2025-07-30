4370d12df8330267079fce647b8843ff75108427
TataKG <tania_1788@list.ru>
2025-07-30 11:17:47 +0300

    Выравнивание данных с предыдущим заданием

--- hw07/src/main/java/ru/otus/hw/repositories/JpaCommentRepository.java
+++ hw07/src/main/java/ru/otus/hw/repositories/JpaCommentRepository.java
@@ -5,7 +5,6 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Comment;
 
-
 import java.util.List;
 import java.util.Optional;
 
