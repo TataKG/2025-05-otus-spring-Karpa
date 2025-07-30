2be2aaff43b4b75a5788d336e31399e26376979a
TataKG <tania_1788@list.ru>
2025-07-30 12:41:15 +0300

    "Бантики"

--- hw07/src/main/java/ru/otus/hw/repositories/GenreRepository.java
+++ hw07/src/main/java/ru/otus/hw/repositories/GenreRepository.java
@@ -3,6 +3,5 @@ package ru.otus.hw.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.hw.models.Genre;
 
-
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
