c53ba8f256c2c2dd3f25d23296ab36dcac35701c
TataKG <tania_1788@list.ru>
2025-10-06 12:26:32 +0300

    Копирование HW10 в качестве основы

---
+++ hw12/src/main/java/ru/otus/hw/controllers/rest/BookController.java
@@ -0,0 +1,77 @@
+package ru.otus.hw.controllers.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.otus.hw.converters.BookDtoConverter;
+import ru.otus.hw.dto.BookDto;
+import ru.otus.hw.dto.BookFormDto;
+import ru.otus.hw.services.AuthorService;
+import ru.otus.hw.services.BookService;
+import ru.otus.hw.services.GenreService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/books")
+@RequiredArgsConstructor
+public class BookController {
+    private final BookService bookService;
+    private final AuthorService authorService;
+    private final GenreService genreService;
+
+    private final BookDtoConverter bookConverter;
+
+    @GetMapping
+    public List<BookDto> getAllBooks() {
+        return bookService.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDto> getBookById(@PathVariable String id) {
+        Optional<BookDto> book = bookService.findById(id);
+        return book.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<BookDto> createBook(@RequestBody BookFormDto bookFormDto) {
+        try {
+            BookDto savedBook = bookService.insert(bookFormDto);
+            return ResponseEntity.ok(savedBook);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<BookDto> updateBook(@RequestBody BookFormDto bookFormDto) {
+        try {
+            BookDto updatedBook = bookService.update(bookFormDto);
+            return ResponseEntity.ok(updatedBook);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable String id) {
+        try {
+            bookService.deleteById(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ hw12/src/main/java/ru/otus/hw/services/BookServiceImpl.java
@@ -0,0 +1,93 @@
+package ru.otus.hw.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.converters.BookDtoConverter;
+import ru.otus.hw.dto.BookDto;
+import ru.otus.hw.dto.BookFormDto;
+import ru.otus.hw.exceptions.EntityNotFoundException;
+import ru.otus.hw.models.Book;
+import ru.otus.hw.repositories.AuthorRepository;
+import ru.otus.hw.repositories.BookRepository;
+import ru.otus.hw.repositories.GenreRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class BookServiceImpl implements BookService {
+    private final AuthorRepository authorRepository;
+
+    private final GenreRepository genreRepository;
+
+    private final BookRepository bookRepository;
+
+    private final BookDtoConverter bookDtoConverter;
+
+    @Override
+    public Optional<BookDto> findById(String id) {
+        return bookRepository.findById(id).map(bookDtoConverter::toDto);
+    }
+
+    @Override
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookDtoConverter::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional
+    public BookDto insert(BookFormDto bookDto) {
+        return save(bookDto);
+    }
+
+    @Override
+    @Transactional
+    public BookDto update(BookFormDto bookDto) {
+        return save(bookDto);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        bookRepository.deleteById(id);
+    }
+
+    private Book prepareBook(BookFormDto bookDto) {
+        Book book;
+        if (bookDto.id() == null || bookDto.id().isEmpty()) {
+            book = new Book();
+        } else {
+            book = bookRepository.findById(bookDto.id()).orElseThrow(
+                    ()
+                            -> new EntityNotFoundException(
+                            "Book with id %s not found".formatted(bookDto.id()))
+            );
+        }
+
+        book.setTitle(bookDto.title());
+
+        var author = authorRepository.findById(bookDto.authorId())
+                .orElseThrow(()
+                        -> new EntityNotFoundException(
+                        "Author with id %s not found"
+                                .formatted(bookDto.authorId())));
+        var genre = genreRepository.findById(bookDto.genreId())
+                .orElseThrow(()
+                        -> new EntityNotFoundException(
+                        "Genre with id %s not found"
+                                .formatted(bookDto.genreId())));
+        book.setAuthor(author);
+        book.setGenre(genre);
+        return book;
+    }
+
+    private BookDto save(BookFormDto bookFormDto) {
+        Book book;
+        book = prepareBook(bookFormDto);
+        return bookDtoConverter.toDto(bookRepository.save(book));
+    }
+}
